6dc99b2522956dc31b1bd12827e776e3c67fa2d2
shewchyk <shewchykj@gmail.com>
2024-03-30 09:09:40 +0100

    First

---
+++ cleandl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/andrew/Downloads
+mv *.jpg /home/andrew/Pictures
+mv *Screenshot* /home/andrew/Pictures/Screenshots
+mv *.pdf /home/andrew/Documents/Teaching/Online_Lessons
+cd /home/andrew/Downloads
+pwd
+ls -1
