772fc44fbbd571951b24cf149980bd5fdf4772cf
shewchyk <shewchykj@gmail.com>
2024-04-09 19:54:29 +0200

    param updates

--- mvinvoices.sh
+++ mvinvoices.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-echo Enter earning month: 
-read MONTH
 cd /home/andrew/Documents/Teaching/Admin/Invoices/2024/
-mkdir $MONTH
-cd /home/andrew/Documents/Teaching/Admin/Invoices/2024/$MONTH
+mkdir $1
+cd /home/andrew/Documents/Teaching/Admin/Invoices/2024/$1
 #creates folder for "proof of payment" (pop) bank statments that are downloaded as invoices are paid
 mkdir -p POP
 cd /home/andrew/Downloads/
-mv *Justin* /home/andrew/Documents/Teaching/Admin/Invoices/2024/$MONTH
-cd /home/andrew/Documents/Teaching/Admin/Invoices/2024/$MONTH
+mv *Justin* /home/andrew/Documents/Teaching/Admin/Invoices/2024/$1
+cd /home/andrew/Documents/Teaching/Admin/Invoices/2024/$1
 pwd
 ls -1
--- mvmass.sh
+++ mvmass.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 cd /home/andrew/Downloads
-echo Move files containing: 
-read FILE
-echo Where is it being moved: 
-read LOCATION
-if ["$LOCATION" = "pictures"]; then 
-    mv *$FILE* /home/andrew/Pictures
-    cd /home/andrew/Pictures
-    pwd
-    ls -1
-fi
-if ["$LOCATION" = "screenshots"]; then 
-    mv *$FILE* /home/andrew/Pictures/Screenshots
-    cd /home/andrew/Pictures/Screenshots
-    pwd
-    ls -1
-fi
-if ["$LOCATION" = "lesson pdfs"]; then 
-    mv *$FILE* /home/andrew/Documents/Teaching/Online_Lessons
-    cd /home/andrew/Documents/Teaching/Online_Lessons
-    pwd
-    ls -1
-fi
+mv *$1* /home/andrew/$2
+pwd
+ls -1
